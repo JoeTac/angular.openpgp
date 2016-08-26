88e843fb93b297daee3ff4732132b703696d1118
JoeTac <jtacconelli@gmail.com>
2016-08-26 13:30:26 -0400

    Update angular.OpenPGP.js
    Return angular promise instead of OpenPGP.js version of a promise

--- angular.OpenPGP.js
+++ angular.OpenPGP.js
@@ -2,26 +2,42 @@
 	var module = angular.module("OpenPGP", []);
 	var defaultKeySize = 4096;
 
-	module.factory("$pgp", ["$window", function($window) {
-		return new OpenPGP($window);
+	module.factory("$pgp", ["$window", "$q", function($window, $q) {
+		return new OpenPGP($window, $q);
 	}]);
 
 	module.provider('$pgpProvider', function () {
-        this.$get = ["$window", function OpenPgpFactory($window) {
-            return new OpenPGP($window);
+        this.$get = ["$window", "$q", function OpenPgpFactory($window, $q) {
+            return OpenPGP($window, $q);
         }];
     });
 
-    function OpenPGP($window) {
+    function OpenPGP($window, $q) {
     	var openpgp = $window.openpgp;
     	return {
+    		keydata: function(pubkey) {
+    			var user = openpgp.key.readArmored(pubkey).keys[0].users[0].userId.userid.split(" ");
+    			return {
+    				name: user[0],
+    				email: user[1].replace("<", "").replace(">", "")
+    			};
+    		},
 			encrypt: function(message, pubkeys) {
 				var options = {
 				    data: message,
 				    publicKeys: openpgp.key.readArmored(pubkeys).keys
 				};
 
-				return openpgp.encrypt(options);
+				var deferred = $q.defer();
+				openpgp.encrypt(options)
+					.then(function(response) {
+						deferred.resolve(response.data);
+					})
+					.catch(function(error) {
+						deferred.resolve(error);
+					});
+
+				return deferred.promise;
 			},
 			decrypt: function(message, privkey, passphrase) {
 				var options = {
@@ -34,7 +50,15 @@
 					options['privateKey'] = key.keys[0];
 				}
 
-				return openpgp.decrypt(options);
+				var deferred = $q.defer();
+				openpgp.decrypt(options)
+				.then(function(response) {
+					deferred.resolve(response.data);
+				})
+				.catch(function(error) {
+					deferred.reject(error);
+				});
+				return deferred.promise;
 			},
 			keygen: function(name, email, passphrase, keySize) {
 				if (!keySize || isNaN(keySize) || keySize<=0 ) {
@@ -56,7 +80,18 @@
 					options['passphrase'] = passphrase;
 				}
 
-				return openpgp.generateKey(options);
+				var deferred = $q.defer();
+				openpgp.generateKey(options)
+				.then(function(response) {
+					deferred.resolve({
+						pubkey: response.publicKeyArmored,
+						privkey: response.privateKeyArmored
+					});
+				})
+				.catch(function(response) {
+					deferred.reject(response);
+				});
+				return deferred.promise;
 			}
 		};
     }
